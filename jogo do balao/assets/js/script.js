e965dfc4978f91600d363362835857609d8ec048
Junior331 <qualquercoisa333133127@gmail.com>
2019-02-05 18:53:56 -0200

    aprendendo js

--- jogo do balao/assets/js/script.js
+++ jogo do balao/assets/js/script.js
@@ -1,34 +1,42 @@
-function addBalao() {
-    var img = document.getElementById("img");
-    var p1 = Math.floor(Math.random() * 500);
-    var p2 = Math.floor(Math.random() * 400);
-    img.setAttribute("style", "left:" + p1 + "px;top:" + p2 + "px;");
-    img.setAttribute("onclick", "esplodir(this)");
-    document.body.appendChild(img);
-    placar();
+ function addBalao() {
+    var Img = document.createElement("img");
+    Img.setAttribute('src', 'https://pngimage.net/wp-content/uploads/2018/05/bal%C3%A3o-roxo-png.png');
+    Img.setAttribute('height', '75px');
+    Img.setAttribute('width', '75px');
+    var p1 = Math.floor(Math.random() * 900);
+    var p2 = Math.floor(Math.random() * 500);
+    Img.setAttribute("style","left:"+p1+"px;top:"+p2+"px;");
+    Img.setAttribute("onclick", "esplodir(this)");
+    Img.style.position = 'absolute';
+
+    document.body.appendChild(Img);
 }
 
 function esplodir(elemento) {
     document.body.removeChild(elemento);
+    placar();
+
 }
 
 function start() {
     setInterval(addBalao, 1000);
 }
-function pause() {
-    alert("Para voltar ao jogo aperte OK");
+
+
+function pause(elemento) {
+    alert("Para voltar ao jogo aperte em OK");
 }
 
 function placar() {
     setTimeout(function () {
-        var hits = document.querySelector(".hits");
-        var valorInteiro = parseInt(hits.textContent);
+        var pontos = document.querySelector(".pontos");
+        var valorInteiro = parseInt(pontos.textContent);
         var soma = parseInt(valorInteiro) + parseInt(01);
-        hits.textContent = soma;
+        pontos.textContent = soma;
     }, 1);
-    var hits = document.querySelector(".hits");
-    hits.classList.add("animationContador");
+    var pontos = document.querySelector(".pontos");
+    pontos.classList.add("animationContador");
     setTimeout(function () {
-        hits.classList.remove("animationContador")
+        pontos.classList.remove("animationContador")
     }, 440)
 }
